7fe03b4b2baceeb6d74a0abbdd2abda7d327704c
aleksandrbogdanov <am.bogdanov@gmail.com>
2022-02-07 20:47:32 +0600

    Just another commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import React, {useEffect} from 'react';
-import GitHubStore from "@gitHubStore";
+import React from 'react';
+import './root/root';
 function App() {
-  const gitHubStore = new GitHubStore();
-  const EXAMPLE_ORGANIZATION:string = 'ktsstudio';
-  const authToken:string = 'PASTE_YOUR_TOKEN_HERE';
-  const repoName:string = 'testRepo';
-  const repoIsPrivate:boolean = true;
-
-
-
-  useEffect(() => {
-    gitHubStore.getOrganizationReposList({
-      organizationName: EXAMPLE_ORGANIZATION
-    }).then(result => {
-      console.log(result); // в консоли появится список репозиториев в ktsstudio
-    })
-    gitHubStore.createRepoAuthUser({name:repoName, isPrivate:repoIsPrivate, authToken: authToken}).then(result => {
-      console.log(result); // в консоли появятся параметры нового репозитория
-    })
-  })
-
-  return (
-    <div className="App">
-    </div>
-  );
+    return <div className='App'></div>;
 }
 
 export default App;
--- src/root/root.ts
+++ src/root/root.ts
@@ -1,15 +1,24 @@
-// Здесь необходимо продемонстрировать создание и использование GitHubStore
-
 import GitHubStore from '../store/GitHubStore/GitHubStore';
 
 const gitHubStore = new GitHubStore();
+const EXAMPLE_ORGANIZATION: string = 'ktsstudio';
+const authToken: string = 'PASTE_YOUR_TOKEN_HERE';
+const repoName: string = 'testRepo';
+const repoIsPrivate: boolean = true;
 
-const EXAMPLE_ORGANIZATION = 'ktsstudio';
-
-gitHubStore.getOrganizationReposList({
-  organizationName: EXAMPLE_ORGANIZATION
-}).then(result => {
-  console.log(result); // в консоли появится список репозиториев в ktsstudio
-})
-
-// В ДЗ 1 Не требуется визуально в разметке отображать результат запроса к сети. Достаточно вывести в console.log
+gitHubStore
+    .getOrganizationReposList({
+        organizationName: EXAMPLE_ORGANIZATION,
+    })
+    .then((result) => {
+        console.log(result); // в консоли появится список репозиториев в ktsstudio
+    });
+gitHubStore
+    .createRepoAuthUser({
+        name: repoName,
+        isPrivate: repoIsPrivate,
+        authToken: authToken,
+    })
+    .then((result) => {
+        console.log(result); // в консоли появятся параметры нового репозитория
+    });
